2d584d0df58db7667ab71e25b5bf6c595ad57e46
AndrewTjandra <andrew.tjandra@gmail.com>
2019-10-01 13:39:06 -0700

    Update MONGOURI connection string to accept env variable

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/TrainingRegistration', { useNewUrlParser: true });
+const mongoURI = process.env.MONGOURI || 'mongodb://localhost:27017/TrainingRegistration'
+
+mongoose.connect(mongoURI, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.once('error', () => console.log('mongoose connection error'));
